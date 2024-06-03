de8bb04cb688fe180c4b7280347865a6867d3c03
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2024-06-03 23:51:14 +0200

    implement logic for daily weather

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import "./style.css";
-import {getWeather} from "./weather.js";
+import { getWeather } from "./weather.js";
 import { ICON_MAP } from "./iconMap.js";
 
-const data = await getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone);
+const data = await getWeather(
+  10,
+  10,
+  Intl.DateTimeFormat().resolvedOptions().timeZone
+);
 console.log(data);
 renderWeather(data);
 
-function renderWeather({current, daily, hourly}) {
+function renderWeather({ current, daily, hourly }) {
   renderCurrentWeather(current);
+  renderDailyWeather(daily);
 }
 
 function renderCurrentWeather(current) {
-  document.querySelector("[data-current-icon]").src = getIconUrl(current.iconCode);
+  document.querySelector("[data-current-icon]").src = getIconUrl(
+    current.iconCode
+  );
   setValue("current-temp", current.currentTemp);
   setValue("current-high", current.highTemp);
   setValue("current-low", current.lowTemp);
@@ -21,10 +28,28 @@ function renderCurrentWeather(current) {
   setValue("current-precip", current.precip);
 }
 
-function setValue(selector, value, {parent = document} = {}) {
+function renderDailyWeather(daily) {
+  const dailySection = document.querySelector("[data-day-section]");
+  const dayCardTemplate = document.getElementById("day-card-template");
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", formatDate(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
+}
+
+function setValue(selector, value, { parent = document } = {}) {
   parent.querySelector(`[data-${selector}]`).textContent = value;
 }
 
 function getIconUrl(iconCode) {
   return `icons/${ICON_MAP.get(iconCode)}.svg`;
+}
+
+function formatDate(timestamp) {
+  const date = new Date(timestamp * 1000);
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
 }
