3e2a80575dd9ad424648135cda4bff6305b78e88
WarioNeila86 <6084473+WarioNeila86@users.noreply.github.com>
2024-06-03 23:39:05 +0200

    use templates in html, retrieve weather info from API, implement logic to display current weather

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 import "./style.css";
+import {getWeather} from "./weather.js";
+import { ICON_MAP } from "./iconMap.js";
+
+const data = await getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone);
+console.log(data);
+renderWeather(data);
+
+function renderWeather({current, daily, hourly}) {
+  renderCurrentWeather(current);
+}
+
+function renderCurrentWeather(current) {
+  document.querySelector("[data-current-icon]").src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
+
+function setValue(selector, value, {parent = document} = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl(iconCode) {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`;
+}
